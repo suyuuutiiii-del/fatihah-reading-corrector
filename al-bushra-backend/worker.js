04e7a468123e067eaca7416533da61a0706404d2
suyuuutiiii-del <suyuuutiiii@gmail.com>
2026-09-18 20:04:29 +0100

    Add Muslim Welfare family support to FARD matching backend

--- al-bushra-backend/worker.js
+++ al-bushra-backend/worker.js
@@ -1,4 +1,4 @@
-const FARD_SERVICES = ['janazah','transport','new-muslim','quran','practical-care','skills','mutual-aid','general'];
+const FARD_SERVICES = ['janazah','transport','new-muslim','quran','practical-care','skills','mutual-aid','family-welfare','general'];
 
 export default {
   async fetch(request, env) {
